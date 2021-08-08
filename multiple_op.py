66717299e7081b8908927f65519b20841abe745e
Fuentes <og.fuentes555@gmail.com>
2021-08-08 17:49:52 -0600

    metodo 1 version 1 8 agosto

---
+++ multiple_op.py
@@ -0,0 +1,34 @@
+from math import factorial 
+resultados = []
+
+def numero_int(entrada):
+    try:
+        return isinstance(entrada, int)
+    except ValueError:
+        return False
+
+def numero_float(entrada):
+    try:
+        return isinstance(entrada, float)
+    except ValueError:
+        return False
+
+def multiple_op(X):
+    if numero_int(X) and int(X)>=0:
+        resultados.clear()
+        resultados.append(X*X)
+        resultados.append(pow(2,X))
+        resultados.append(factorial(X))
+        print(resultados)
+        
+    elif numero_int(X) and int(X)<0:
+        print ("Código de error 101.\n")
+
+    elif numero_float(X) and float(X)<0:
+        print ("Código de error 102.\n")
+
+    elif numero_float(X) and float(X)>0:
+        print ("Código de error 103.\n")
+
+    else:
+        print("Código de error 3312.\n")
