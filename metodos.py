03b5a0bcaa97f032a0e0a4766d9f155cd2da3fa2
Fuentes <og.fuentes555@gmail.com>
2021-08-09 18:48:21 -0600

    flake8 correccion de errores en los dos archivos

--- metodos.py
+++ metodos.py
@@ -1,8 +1,11 @@
-from math import factorial  # importacion de libreria para factorial
+from math import factorial
+# importacion de libreria para factorial
 
 
-def numero_int(entrada):  # función que verifica que el numero sea entero
-    return isinstance(entrada, int)  # si es entero regresa True
+def numero_int(entrada):
+    # función que verifica que el numero sea entero
+    return isinstance(entrada, int)
+    # si es entero regresa True
 
 
 def numero_float(entrada):
@@ -10,23 +13,22 @@ def numero_float(entrada):
 
 
 def multiple_op(X):
-    """
-    metodo uno, verifica que recibe un numero,
-    realiza operaciones y retorna un arreglo con el resultado.
-    El parametro de entrada es un numero.
-    """
+    # metodo uno, verifica que recibe un numero,
+    # realiza operaciones y retorna un arreglo con el resultado.
+    # El parametro de entrada es un numero.
 
-    resultados_metodo_1 = []  # se inicia el arreglo
+    resultados_metodo_1 = []
+    # se inicia el arreglo
 
     if numero_int(X) and int(X) >= 0:
-        """
-        solo si cumple las condiciones de ser
-        numero entero positivo procede
-        """
+        # solo si cumple las condiciones de ser
+        # numero entero positivo procede
+
         resultados_metodo_1.append(X*X)
         resultados_metodo_1.append(pow(2, X))
         resultados_metodo_1.append(factorial(X))
-        return resultados_metodo_1  # retorno del arreglo final
+        return resultados_metodo_1
+        # retorno del arreglo final
 
     elif numero_int(X) and int(X) < 0:
         print("Código de error 1010.\n")
@@ -42,19 +44,19 @@ def multiple_op(X):
 
 
 def verify_array_op(arreglo):
-    """
-    Metodo dos, verifica que recibe solo numeros,
-    llama al metodo 1 y retorna un arreglo con el
-    resultado. El parametro de entrada es un arreglo.
-    """
+    # Metodo dos, verifica que recibe solo numeros,
+    # llama al metodo 1 y retorna un arreglo con el
+    # resultado. El parametro de entrada es un arreglo.
+
     resultados_metodo_2 = []
-    largo = len(arreglo)  # cantidad de elementos del arreglo
+    largo = len(arreglo)
+    # cantidad de elementos del arreglo
+
     i = 0
     while i < largo:
-        """
-        ciclo que recorre cada elemento del arreglo
-        y llama a la funcion uno enviandolo el dato del arreglo.
-        """
+        # ciclo que recorre cada elemento del arreglo
+        # y llama a la funcion uno enviandolo el dato del arreglo.
+
         if numero_int(arreglo[i]) and int(arreglo[i]) >= 0:
             resultados_metodo_2.append(multiple_op(arreglo[i]))
 
@@ -62,18 +64,14 @@ def verify_array_op(arreglo):
             print("Código de error 3141.\n")
         i += 1
     print(resultados_metodo_2)
-    """
-    una vez que termina la lista imprime
-     el resultado como un arreglo de arreglos.
-    """
+    # una vez que termina la lista imprime
+    # el resultado como un arreglo de arreglos.
 
 
-"""
-Codigos de error:
-1010 #codigo de error para numero menor a cero
-1020 #codigo de error para numero negativo y decimal
-1030 #codigo de error para numero decimal
-3141 #codigo de error para elemento del arreglo que
-no cumple con ser numero entero positivo
-3312 #codigo de error para caracter o cadena de caracteres
-"""
+# Codigos de error:
+# 1010 #codigo de error para numero menor a cero
+# 1020 #codigo de error para numero negativo y decimal
+# 1030 #codigo de error para numero decimal
+# 3141 #codigo de error para elemento del arreglo que
+# no cumple con ser numero entero positivo
+# 3312 #codigo de error para caracter o cadena de caracteres
--- test_metodos.py
+++ test_metodos.py
@@ -4,37 +4,46 @@ import random
 
 from math import factorial
 
+
 def resp_correcta_metodo1(dato):
-    resultados_metodo_1 = [] #se inicia el arreglo 
-    if isinstance(dato, int) and dato>=0: #solo si cumple las condiciones de ser numero entero positivo procede
+    resultados_metodo_1 = []
+    # se inicia el arreglo
+    if isinstance(dato, int) and dato >= 0:
+        # solo si cumple las condiciones de ser numero entero positivo procede
         resultados_metodo_1.append(dato*dato)
-        resultados_metodo_1.append(pow(2,dato))
+        resultados_metodo_1.append(pow(2, dato))
         resultados_metodo_1.append(factorial(dato))
         return resultados_metodo_1
     else:
         return None
 
+
 def resp_correcta_metodo2(arreglo):
     resultados_metodo_2 = []
-    largo = len(arreglo) #cantidad de elementos del arreglo
+    largo = len(arreglo)
+    # cantidad de elementos del arreglo
     i = 0
-    while i < largo : #ciclo que recorre cada elemento del arreglo y llama a la funcion uno enviandolo el dato del arreglo
-        if isinstance(arreglo[i], int) and arreglo[i]>=0:
-            resultados_metodo_2.append(resp_correcta_metodo1(arreglo[i]))    
+    while i < largo:
+        # ciclo que recorre cada elemento del arreglo
+        # y llama a la funcion uno enviandolo el dato del arreglo
+
+        if isinstance(arreglo[i], int) and arreglo[i] >= 0:
+            resultados_metodo_2.append(resp_correcta_metodo1(arreglo[i]))
         else:
             return None
-        i+=1
+        i += 1
+
 
 class TestMetodos(unittest.TestCase):
     def test_add(self):
 
-        prueba = [-1,"a",4,0.2,1,0,5]
-        
+        prueba = [-1, "a", 4, 0.2, 1, 0, 5]
+
         i = random.randint(0, 6)
         result = metodos.multiple_op(prueba[i])
-        self.assertEqual(result,resp_correcta_metodo1(prueba[i]))
-
-        arreglo_prueba = [prueba[random.randint(0, 6)],prueba[random.randint(0, 6)]]     
+        self.assertEqual(result, resp_correcta_metodo1(prueba[i]))
+        entrada_1 = prueba[random.randint(0, 6)]
+        entrada_2 = prueba[random.randint(0, 6)]
+        arreglo_prueba = [entrada_1, entrada_2]
         result = metodos.verify_array_op(arreglo_prueba)
-        self.assertEqual(result,resp_correcta_metodo2(arreglo_prueba))
-
+        self.assertEqual(result, resp_correcta_metodo2(arreglo_prueba))
