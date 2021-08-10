5c1318cb8b8bb616ff107105e266ac7cd3b099f6
Joshua <jcorvera10@hotmail.com>
2021-08-09 23:17:03 -0600

    Test documentado

--- test_metodos.py
+++ test_metodos.py
@@ -1,27 +1,32 @@
 import unittest
 import metodos
 import random
-
+    #Llamado de las librerías a usar. 
 from math import factorial
-
+    #Importación de librerías para el factorial
 
 def resp_correcta_metodo1(dato):
+    #Función para determinar la respuesta correcta del métdo 1
     resultados_metodo_1 = []
-    # se inicia el arreglo
+    # se inicia el arreglo vacío
     if isinstance(dato, int) and dato >= 0:
         # solo si cumple las condiciones de ser numero entero positivo procede
+        #Se crean los resultados que se van a agregar al arreglo
         resultados_metodo_1.append(dato*dato)
         resultados_metodo_1.append(pow(2, dato))
         resultados_metodo_1.append(factorial(dato))
         return resultados_metodo_1
+        #Devuelve el arreglo con los resultados
     else:
         return None
 
 
 def resp_correcta_metodo2(arreglo):
+    # Fución para determinar la respuesta correcta del método 2 (Array)
     resultados_metodo_2 = []
+    # Arreglo vacío para almacenar los resultados 
     largo = len(arreglo)
-    # cantidad de elementos del arreglo
+    # Fijar cantidad de elementos del arreglo
     i = 0
     while i < largo:
         # ciclo que recorre cada elemento del arreglo
@@ -35,15 +40,19 @@ def resp_correcta_metodo2(arreglo):
 
 
 class TestMetodos(unittest.TestCase):
+    # Se crea una clase para realizar el test de los métodos
     def test_add(self):
-
+    # función para realizar el test haciendo uso de número random
         prueba = [-1, "a", 4, 0.2, 1, 0, 5]
+        # Arreglo que hace función de base de datos a probar
 
-        i = random.randint(0, 6)
+        i = random.randint(0, 6)  # Se asigna un valor entre el rango de 0 a 6
         result = metodos.multiple_op(prueba[i])
         self.assertEqual(result, resp_correcta_metodo1(prueba[i]))
         entrada_1 = prueba[random.randint(0, 6)]
         entrada_2 = prueba[random.randint(0, 6)]
+        # Se selecciona una entrada al azar del arreglo prueba
         arreglo_prueba = [entrada_1, entrada_2]
         result = metodos.verify_array_op(arreglo_prueba)
+        # Se verifican los dos datos del arreglo sellecionado de prueba
         self.assertEqual(result, resp_correcta_metodo2(arreglo_prueba))
